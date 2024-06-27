516cab5935d47a3e9293095bd0cc619191858490
ujwala palika <ujwalapalika@gmail.com>
2024-06-27 10:48:07 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const port = process.env.PORT || 5000; // Use the port provided by Railway or default to 5000
+const port = process.env.PORT || 3000; // Use the port provided by Railway or default to 5000
 
 // SQLite setup
 const db = new sqlite3.Database(':memory:'); // In-memory database for demo purposes
